880ead6b5ae6e3e3b0c68b25049aa9868468ecd5
uttamXD <raiuttam88.ur@gmail.com>
2023-06-28 22:04:07 +0545

    first comit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Bottom Nav App",
+      home: MainPage(),
+    ));
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
+  List<Widget> meroWidget = <Widget>[
+    Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Container(
+        height: 100,
+        width: double.infinity,
+        color: Color(0xFF2944F7),
+        child: Center(
+          child: Text(
+            "Welcome to Home",
+            style: TextStyle(fontSize: 50, color: Colors.white),
+          ),
+        ),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text("Welcome to Container"),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text("Welcome to Settings"),
+      ),
+    ),
+  ];
+
+  _onItemTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text("Bottom Navigation Bar"),
+      ),
+      body: Center(
+        child: meroWidget.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: 'About'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings_applications), label: 'Settings'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTap,
+      ),
+    );
+  }
+}
